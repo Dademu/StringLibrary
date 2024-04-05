d1c9ab1b0d84a03357418f399b49ca11cb2c13a5
Dademu <a00278997@mycambrian.ca>
2024-04-05 19:50:24 -0400

    updates

---
+++ StringLibrary/Class1.cs
@@ -0,0 +1,16 @@
+﻿namespace UtilityLibraries;
+
+/// <summary>
+///this class holds the extension method for the string data type
+/// </summary>
+public static class StringLibrary
+{
+    public static bool StartsWithUpper(this string? str)
+    {
+        if (string.IsNullOrWhiteSpace(str))
+            return false;
+
+        char ch = str[0];
+        return char.IsUpper(ch);
+    }
+}
